b8c4c1a0398e990446246d6295550b19e6ce0950
carlos benavides <1992cab@gmail.com>
2023-02-16 21:07:57 -0500

    chore: added employees route by default

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'employees' },
+  {
+    path: 'employees',
+    loadComponent: () =>
+      import(
+        './entities/employees/employees-list/employees-list.component'
+      ).then(mod => mod.EmployeesListComponent),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
