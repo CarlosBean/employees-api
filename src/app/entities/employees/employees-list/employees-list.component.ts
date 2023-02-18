222f4e9b1c278b77a052b4f3420ad68e66aa5171
carlos benavides <1992cab@gmail.com>
2023-02-18 14:00:53 -0500

    feat: added employee list layout

--- src/app/entities/employees/employees-list/employees-list.component.ts
+++ src/app/entities/employees/employees-list/employees-list.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { EmployeeCardComponent } from '../employee-card/employee-card.component';
+import { EmployeesService } from '../employees.service';
 
 @Component({
   selector: 'app-employees-list',
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: './employees-list.component.html'
+  imports: [CommonModule, EmployeeCardComponent],
+  templateUrl: './employees-list.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class EmployeesListComponent {
+  employees$ = this.employeesService.getAllEmployees();
 
+  constructor(private employeesService: EmployeesService) {}
 }
