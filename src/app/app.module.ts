bd7f4ea65923cc1b63b0a0a4b2c05bac516da240
carlos benavides <1992cab@gmail.com>
2023-02-18 13:45:25 -0500

    feat: added employee card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { provideImgixLoader } from '@angular/common';
+import { environment } from 'src/environments/environment';
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
-  providers: [],
+  providers: [provideImgixLoader(environment.API_IMG_URL)],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/entities/employees/employee-card/employee-card.component.ts
@@ -0,0 +1,27 @@
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { Employee } from '../employee.model';
+
+@Component({
+  selector: 'app-employee-card',
+  standalone: true,
+  imports: [CommonModule, NgOptimizedImage],
+  templateUrl: './employee-card.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class EmployeeCardComponent {
+  @Input() data?: Employee;
+
+  images = {
+    young: {
+      name: 'photo-1535295972055-1c762f4483e5?w=400&h=400&fit=crop',
+      width: 400,
+      height: 400,
+    },
+    old: {
+      name: 'photo-1552641156-93ea2a5f78e2?w=400&h=400&fit=crop',
+      width: 400,
+      height: 400,
+    },
+  };
+}
